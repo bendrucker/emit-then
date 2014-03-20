94d58d3802163bd4d790b28600343de9e0f96186
Ben Drucker <bvdrucker@gmail.com>
2014-03-20 16:04:27 -0400

    Test default event handler behavior (args + ctx)

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* globals describe:false, beforeEach:false, it:false, expect:false, sinon:false */
+
 require('test-setup');
 
 var EventEmitter = require('events').EventEmitter;
@@ -19,12 +21,17 @@ describe('emitThen', function () {
     deferred = Promise.defer();
     emitter.on('event', function () {
       return deferred.promise;
-    })
+    });
   });
 
   var error;
   beforeEach(function () {
     error = new Error('A wild error appeared');
+  });
+
+  var spy;
+  beforeEach(function () {
+    spy = sinon.spy();
   });
 
   it('resolves when the handlers resolve', function () {
@@ -45,6 +52,22 @@ describe('emitThen', function () {
     });
     return expect(emitter.emitThen('event'))
       .to.be.rejectedWith(error);
+  });
+
+  it('preserves the context for the handlers', function () {
+    deferred.resolve();
+    emitter.on('event', spy);
+    return emitter.emitThen('event').finally(function () {
+      expect(spy).to.have.been.calledOn(emitter);
+    });
+  });
+
+  it('preserves the arguments for the handlers', function () {
+    deferred.resolve();
+    emitter.on('event', spy);
+    return emitter.emitThen('event', 'foo', 'bar').finally(function () {
+      expect(spy).to.have.been.calledWith('foo', 'bar');
+    });
   });
 
   it('can register itself on the native emitter prototype', function () {
