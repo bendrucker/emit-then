5a3938b40bf6ca75414844f82e14292bcfcd39e5
Ben Drucker <bvdrucker@gmail.com>
2014-10-11 12:53:54 -0400

    Missing semicolon

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function emitThen (event) {
       var a1 = args[0], a2 = args[1];
       switch (args.length) {
         case 0: return listener.call(this);
-        case 1: return listener.call(this, a1)
+        case 1: return listener.call(this, a1);
         case 2: return listener.call(this, a1, a2);
         default: return listener.apply(this, args);
       }
