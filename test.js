9abbe56550098db02d432f060bfee02b35e0a78b
Ben Drucker <bvdrucker@gmail.com>
2014-10-11 12:47:44 -0400

    Test cleanup

--- test.js
+++ test.js
@@ -1,55 +1,47 @@
 'use strict';
 
-/* globals describe:false, beforeEach:false, it:false, expect:false, sinon:false */
+/* globals describe:false, beforeEach:false, it:false */
 /* jshint expr:true */
 
-require('test-setup');
-
+var chai         = require('chai');
+var expect       = chai.expect;
+var sinon        = require('sinon');
 var EventEmitter = require('events').EventEmitter;
 var Promise      = require('bluebird');
 var emitThen     = require('./');
 
+chai
+  .use(require('chai-as-promised'))
+  .use(require('sinon-chai'));
+
 describe('emit-then', function () {
 
-  var emitter;
+  var emitter, error, spy;
   beforeEach(function () {
     emitter = new EventEmitter();
     emitter.emitThen = emitThen;
-  });
-
-  var deferred;
-  beforeEach(function () {
-    deferred = Promise.defer();
-    emitter.on('event', function () {
-      return deferred.promise;
-    });
-  });
-
-  var error;
-  beforeEach(function () {
     error = new Error('A wild error appeared');
-  });
-
-  var spy;
-  beforeEach(function () {
     spy = sinon.spy();
   });
 
   it('resolves when the handlers resolve', function () {
-    process.nextTick(deferred.resolve.bind(deferred, 'foo'));
+    emitter.on('event', function () {
+      return Promise.resolve();
+    });
     return emitter.emitThen('event').then(function (value) {
       expect(value).to.be.null;
     });
   });
 
   it('rejects when a handler rejects', function () {
-    process.nextTick(deferred.reject.bind(deferred, error));
+    emitter.on('event', function () {
+      return Promise.reject(error);
+    });
     return expect(emitter.emitThen('event'))
       .to.be.rejectedWith(error);
   });
 
   it('rejects when a handler throws', function () {
-    deferred.resolve();
     emitter.on('event', function () {
       throw error;
     });
@@ -58,7 +50,6 @@ describe('emit-then', function () {
   });
 
   it('preserves the context for the handlers', function () {
-    deferred.resolve();
     emitter.on('event', spy);
     return emitter.emitThen('event').finally(function () {
       expect(spy).to.have.been.calledOn(emitter);
@@ -66,7 +57,6 @@ describe('emit-then', function () {
   });
 
   it('preserves the arguments for the handlers', function () {
-    deferred.resolve();
     emitter.on('event', spy);
     return emitter.emitThen('event', 'foo', 'bar').finally(function () {
       expect(spy).to.have.been.calledWith('foo', 'bar');
