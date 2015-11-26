184a07a39528075bbe340edcc2b8dae003daae70
Pelle Wessman <pelle@kodfabrik.se>
2015-11-26 16:53:57 +0100

    Update to use native Promise

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 'use strict';
 
 var EventEmitter = require('events').EventEmitter;
-var Promise      = require('bluebird');
-
 function emitThen (event) {
   var args = Array.prototype.slice.call(arguments, 1);
   /* jshint validthis:true */
   return Promise
-    .bind(this)
-    .return(this)
-    .call('listeners', event)
-    .map(function (listener) {
+    .all(this.listeners(event).map(listener => Promise.resolve().then(() => {
       var a1 = args[0], a2 = args[1];
       switch (args.length) {
         case 0: return listener.call(this);
@@ -18,8 +13,8 @@ function emitThen (event) {
         case 2: return listener.call(this, a1, a2);
         default: return listener.apply(this, args);
       }
-    })
-    .return(null);
+    })))
+    .then(() => null);
 }
 
 emitThen.register = function () {
