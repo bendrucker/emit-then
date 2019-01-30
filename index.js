c214861c775c247010011b88fd3a457d5ca2a7a6
himself65 <himself6565@gmail.com>
2019-01-30 19:10:53 +0800

    [update]

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-'use strict';
-
-function emitThen (event) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  /* jshint validthis:true */
-  return Promise
-    .all(this.listeners(event).map(listener => Promise.resolve().then(() => {
-      var a1 = args[0], a2 = args[1];
-      switch (args.length) {
-        case 0: return listener.call(this);
-        case 1: return listener.call(this, a1);
-        case 2: return listener.call(this, a1, a2);
-        default: return listener.apply(this, args);
-      }
-    })))
-    .then(() => null);
+module.exports.emitThen = async function emitThen (event, ...args) {
+  return Promise.all(this.listeners(event).map(listener => Promise.resolve().then(() => {
+    const _0 = args[0], _1 = args[1]
+    switch (args.length) {
+      case 0:
+        return listener.call(this)
+      case 1:
+        return listener.call(this, _0)
+      case 2:
+        return listener.call(this, _0, _1)
+      default:
+        return listener.apply(this, args)
+    }
+  })))
 }
 
-emitThen.register = function () {
-  require('events').prototype.emitThen = emitThen;
-};
+module.exports.register = function () {
+  require('events').EventEmitter.prototype.emitThen = module.exports.emitThen
+}
 
-module.exports = emitThen;
--- test.js
+++ test.js
@@ -1,70 +1,53 @@
-'use strict';
+/* eslint-env jest */
+const EventEmitter = require('events').EventEmitter
+require('./').register()
 
-/* globals describe:false, beforeEach:false, it:false */
-/* jshint expr:true */
+describe('emit-then unit test', () => {
+  let emitter, error, spy
 
-var chai         = require('chai');
-var expect       = chai.expect;
-var sinon        = require('sinon');
-var EventEmitter = require('events').EventEmitter;
-var emitThen     = require('./');
+  beforeEach(() => {
+    emitter = new EventEmitter()
+    error = new Error('A wild error appeared')
+    spy = jest.fn((...args) => args)
+  })
 
-chai
-  .use(require('chai-as-promised'))
-  .use(require('sinon-chai'));
+  it('resolves when the handlers resolve', (done) => {
+    emitter.on('event', () => {
+      return Promise.resolve(0)
+    })
+    emitter.emitThen('event').then(res => {
+      expect(res).toEqual([0])
+      done()
+    })
+  })
 
-describe('emit-then', function () {
+  it('rejects when a handler rejects', async () => {
+      emitter.on('event', () => Promise.reject(error))
+      return expect(emitter.emitThen('event'))
+        .rejects.toEqual(error)
+    }
+  )
 
-  var emitter, error, spy;
-  beforeEach(function () {
-    emitter = new EventEmitter();
-    emitter.emitThen = emitThen;
-    error = new Error('A wild error appeared');
-    spy = sinon.spy();
-  });
-
-  it('resolves when the handlers resolve', function () {
-    emitter.on('event', function () {
-      return Promise.resolve();
-    });
-    return emitter.emitThen('event').then(function (value) {
-      expect(value).to.be.null;
-    });
-  });
-
-  it('rejects when a handler rejects', function () {
-    emitter.on('event', function () {
-      return Promise.reject(error);
-    });
+  it('rejects when a handler throws', () => {
+    emitter.on('event', () => {
+      throw error
+    })
     return expect(emitter.emitThen('event'))
-      .to.be.rejectedWith(error);
-  });
+      .rejects.toBe(error)
+  })
 
-  it('rejects when a handler throws', function () {
-    emitter.on('event', function () {
-      throw error;
-    });
-    return expect(emitter.emitThen('event'))
-      .to.be.rejectedWith(error);
-  });
+  it('preserves the context for the handlers', async () => {
+    emitter.on('event', spy)
+    await emitter.emitThen('event').then(() => {
+      expect(spy).toHaveBeenCalled()
+    })
+  })
 
-  it('preserves the context for the handlers', function () {
-    emitter.on('event', spy);
-    return emitter.emitThen('event').catch().then(function () {
-      expect(spy).to.have.been.calledOn(emitter);
-    });
-  });
-
-  it('preserves the arguments for the handlers', function () {
-    emitter.on('event', spy);
-    return emitter.emitThen('event', 'foo', 'bar').catch().then(function () {
-      expect(spy).to.have.been.calledWith('foo', 'bar');
-    });
-  });
-
-  it('can register itself on the native emitter prototype', function () {
-    emitThen.register();
-    expect(EventEmitter).to.respondTo('emitThen');
-  });
-
-});
+  it('preserves the arguments for the handlers', (done) => {
+    emitter.on('event', spy)
+    emitter.emitThen('event', 'foo', 'bar').then(() => {
+      expect(spy).toReturnWith(['foo', 'bar'])
+      done()
+    })
+  })
+})
